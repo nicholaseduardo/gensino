7a67e615ea80518941dcfe1bfc9c39d7ffbf6cb0
Nicholas Eduardo Lopes dos Santos <nicholaseduardo@yahoo.com.br>
2019-11-27 11:28:41 -0300

    Create BeanFactory.java
    Interface criada para implementar o padrão factory method para os Beans do projeto.

---
+++ src/ensino/patterns/factory/BeanFactory.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ensino.patterns.factory;
+
+import java.util.HashMap;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author nicho
+ */
+public interface BeanFactory<T> {
+    /**
+     * Construtor padrão.
+     * Método utilizado para criar um objeto de acordo com
+     * seus atributos de instância
+     * @param args  Lista de Parâmetros de acordo com os atributos 
+     *              de instância da classe
+     * @return 
+     */
+    public T getObject(Object ...args);
+    /**
+     * Construtor por elementos do XML.
+     * Método utilizado para criar um objeto a partir
+     * de atributos do arquivo XML.
+     * @param e     Instância da classe <code>Element</code> que contém
+     *              os dados a serem atribuídos à classe
+     * @return 
+     */
+    public T getObject(Element e);
+    /**
+     * Construtor por elementos de um HahsMap.
+     * Método utilizado para criar um objeto a partir
+     * dos dados armazenados no HashMap
+     * @param p     Instância da classe <code>HashMap</code>. Cada item
+     *              adicionado representa um atributo de instância da classe
+     *              a ser criada
+     * @return 
+     */
+    public T getObject(HashMap<String, Object> p);
+}
