7e978522ef7505bdefc5405cc7a55250ef2bf88e
Nicholas Eduardo Lopes dos Santos <nicholaseduardo@yahoo.com.br>
2019-11-27 16:25:08 -0300

    Update CampusDaoXML.java
    ajustes dos métodos

--- src/ensino/configuracoes/dao/xml/CampusDaoXML.java
+++ src/ensino/configuracoes/dao/xml/CampusDaoXML.java
@@ -6,68 +6,44 @@
 package ensino.configuracoes.dao.xml;
 
 import ensino.configuracoes.model.Campus;
+import ensino.configuracoes.model.CampusFactory;
+import ensino.connection.AbstractDaoXML;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 /**
  * Os dados relacionados aos campus estão todos no arquivo de configurações
  *
  * @author nicho
  */
-public class CampusDaoXML extends ConfiguracaoDaoXML<Campus> {
+public class CampusDaoXML extends AbstractDaoXML<Campus> {
 
     public CampusDaoXML() throws IOException, ParserConfigurationException, TransformerException {
-        super("Campus", "campus");
+        super("campus", "Campus", "campus", CampusFactory.getInstance());
     }
 
     @Override
-    public List<Campus> list(String criteria) {
-        loadXmlFile();
-        if (getDoc() == null) {
-            return null;
-        }
-        String expression = criteria;
-        if ("".equals(criteria)) {
-            expression = String.format("/%s%s/%s",
-                    getPathObject(), getXmlGroup(), getNodeName());
-        }
-
-        List<Campus> list = new ArrayList<>();
-        Object searched = getDataExpression(expression);
-        if (searched != null && searched instanceof NodeList) {
-            NodeList nodeList = (NodeList) searched;
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                list.add(new Campus((Element) node));
-            }
-        }
-
-        return list;
+    public Campus findById(Object... ids) {
+        return super.findById(ids[0]);
     }
 
-    /**
-     * Recupera os dados do campus
-     *
-     * @param id Identificação do campus
-     * @return Objeto da classe <code>Campus</code>
-     */
     @Override
-    public Campus findById(Object id) {
-        loadXmlFile();
-        // Cria mecanismo para buscar o conteudo no xml
-        String expression = String.format("/%s%s/%s[@id=%d]",
-                getPathObject(), getXmlGroup(), getNodeName(), id);
-        Node searched = getDataByExpression(expression);
-        if (searched != null) {
-            return new Campus((Element) searched);
-        }
-        return null;
+    public void save(Campus o) {
+        String expression = String.format("@id=%d", o.getId());
+        super.save(o, expression);
+    }
+
+    @Override
+    public void delete(Campus o) {
+        String filter = String.format("@id=%d", o.getId());
+        super.delete(filter);
+    }
+
+    @Override
+    public Integer nextVal() {
+        String expression = String.format("%s/@id", getRootExpression());
+        return super.nextVal(expression);
     }
 
 }
