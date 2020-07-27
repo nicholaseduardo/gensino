f5570f585e78c6812366f809d9bbd6d4959f8f7e
Nicholas Eduardo Lopes dos Santos <nicholaseduardo@yahoo.com.br>
2020-07-27 08:13:45 -0400

    Adapter criado para auxiliar a aplicação de Placeholder do GenJText

--- src/main/java/ensino/components/listener/GenFocusAdapter.java
+++ src/main/java/ensino/components/listener/GenFocusAdapter.java
@@ -5,6 +5,7 @@
  */
 package ensino.components.listener;
 
+import ensino.components.GenJTextField;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import javax.swing.JTextField;
@@ -14,9 +15,14 @@ import javax.swing.JTextField;
  * @author nicho
  */
 public class GenFocusAdapter implements FocusListener {
-
+    
     @Override
     public void focusGained(FocusEvent e) {
+        if (e.getSource() instanceof GenJTextField) {
+            GenJTextField source = (GenJTextField) e.getSource();
+            if (source.isSetPlaceholderText() && !source.isTextWrittenIn())
+                source.setText("");
+        }
         if (e.getSource() instanceof JTextField) {
             JTextField text = (JTextField)e.getSource();
             text.selectAll();
@@ -25,7 +31,13 @@ public class GenFocusAdapter implements FocusListener {
 
     @Override
     public void focusLost(FocusEvent e) {
-        
+        if (e.getSource() instanceof GenJTextField) {
+            GenJTextField source = (GenJTextField) e.getSource();
+            if ("".equals(source.getText()) ||
+                    source.getText().trim().length() == 0) {
+                source.customizeText();
+            }
+        }
     }
     
 }
