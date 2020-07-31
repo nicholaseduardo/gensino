3757ee939f76e805702bbe4af378789064d58952
Nicholas Eduardo Lopes dos Santos <nicholaseduardo@yahoo.com.br>
2020-07-31 10:37:35 -0400

    Ajuste na apresentação dos dados na tabela

--- src/main/java/ensino/configuracoes/view/renderer/ConteudoCellRenderer.java
+++ src/main/java/ensino/configuracoes/view/renderer/ConteudoCellRenderer.java
@@ -7,15 +7,11 @@ package ensino.configuracoes.view.renderer;
 
 import ensino.components.renderer.GenCellRenderer;
 import ensino.components.GenJLabel;
-import ensino.configuracoes.model.Campus;
-import ensino.configuracoes.view.models.CampusTableModel;
+import ensino.configuracoes.model.Conteudo;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 
@@ -23,7 +19,7 @@ import javax.swing.JTable;
  *
  * @author nicho
  */
-public class CampusCellRenderer extends GenCellRenderer {
+public class ConteudoCellRenderer extends GenCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value,
@@ -38,35 +34,45 @@ public class CampusCellRenderer extends GenCellRenderer {
                             : new Color(240, 240, 240)));
         }
 
-        CampusTableModel model = (CampusTableModel) table.getModel();
-        Campus campus = (Campus) model.getRow(row);
-        GenJLabel lblTitle = createLabel(campus.getNome());
-        if (campus.isVigente()) {
+        if (value instanceof Conteudo) {
+            Conteudo c = (Conteudo) value;
+            GenJLabel lblTitle = createLabel("");
+            
+            int n = c.getNivel() != null && c.getNivel() > 1 ? c.getNivel() - 1 : 0;
+            String t = "--".repeat(n).concat(c.getDescricao());
+            
+            lblTitle.setText(t, 60+(n*2));
+            lblTitle.setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
             lblTitle.toBold();
-            lblTitle.setIcon(new ImageIcon(getClass().getResource("/img/check-tick-icon-15px.png")));
+
+            String conteudoParent = "";
+            if (c.hasParent()) {
+                conteudoParent = c.getConteudoParent().getDescricao();
+            }
+
+            GenJLabel lblParent = createLabel("");
+            lblParent.setText(conteudoParent, 60);
+            lblParent.resetFontSize(12);
+            
+            GenJLabel lblNivel = createLabel(
+                    String.format("Nivel: %d / Sequência: %d",
+                            c.getNivel(), c.getSequencia()));
+            lblNivel.resetFontSize(12);
+
+            JPanel panel = createPanel(new BorderLayout());
+            panel.add(lblTitle, BorderLayout.CENTER);
+            panel.add(lblNivel, BorderLayout.LINE_END);
+            panel.add(lblParent, BorderLayout.PAGE_END);
+
+            int height = panel.getPreferredSize().height;
+            if (height < 55) {
+                height = 55;
+            }
+
+            table.setRowHeight(height);
+            panel.setOpaque(true);
+            return panel;
         }
-        lblTitle.setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
-
-        GenJLabel lblCurso = createLabel(String.format("Cursos: %d", campus.getCursos().size()), JLabel.RIGHT);
-        lblCurso.resetFontSize(12);
-        lblCurso.setIcon(new ImageIcon(getClass().getResource("/img/courses-icon-15px.png")));
-
-        GenJLabel lblCal = createLabel(String.format("Calendários: %d", campus.getCalendarios().size()), JLabel.RIGHT);
-        lblCal.resetFontSize(12);
-        lblCal.setIcon(new ImageIcon(getClass().getResource("/img/calendar-image-png-15px.png")));
-
-        JPanel panelIcons = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 0));
-        panelIcons.add(lblCurso);
-        panelIcons.add(lblCal);
-        panelIcons.setBackground(getBack());
-
-        JPanel panel = new JPanel(new GridLayout(2, 1, 10, 0));
-        panel.add(lblTitle);
-        panel.add(panelIcons);
-        panel.setBackground(getBack());
-
-        table.setRowHeight(panel.getPreferredSize().height + 10);
-        panel.setOpaque(true);
-        return panel;
+        return null;
     }
 }
